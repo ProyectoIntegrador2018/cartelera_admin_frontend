69a1c9cd1028d009e5164e253279c88471456146
Luis Carlos Flores <luiscfgmay294@gmail.com>
2018-09-04 17:19:39 -0500

    Fix[configuration]: Update pacakge.json for production

--- webpack.dev.js
+++ webpack.dev.js
@@ -16,6 +16,6 @@ module.exports = merge(common, {
         historyApiFallback: true,
         host: '0.0.0.0',
         disableHostCheck: true,
-        port: (process.env.PORT || 4200)
+        port: (process.env.PORT || 4000)
     }
 })
