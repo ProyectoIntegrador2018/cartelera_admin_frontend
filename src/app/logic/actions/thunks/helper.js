2c470acf13338bf0a6b5976fcd0fd5ad8328892d
Luis Carlos Flores <luiscfgmay294@gmail.com>
2018-11-07 14:51:33 -0600

    Fix api use in production

--- src/app/logic/actions/thunks/helper.js
+++ src/app/logic/actions/thunks/helper.js
@@ -5,7 +5,11 @@ import { createAction } from 'Logic/actions'
 export const request = (
     axios.create({
         //baseURL: 'https://5a8e3738b5a3130012909abb.mockapi.io/api',
-        baseURL: 'http://localhost:3000'
+        if(process.env.NODE_ENV === 'development') {
+            baseURL: 'http://localhost:3000'
+        } else {
+            baseURL: 'http://cartelerai-api.herokuapp.com'
+        }
     })
 )
 
