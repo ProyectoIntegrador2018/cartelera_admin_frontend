80f411cb34310ea0b34de6ad86960552c2e70b0d
Diego JV <diegojosevilla@outlook.com>
2018-11-09 09:52:22 -0600

    Create Applicant Layout

---
+++ src/app/components/presentational/layout/Applicant.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Layout from './MainLayout'
+import { EventsLink, ProfileLinks } from './Links'
+import { EventsRoutes, ProfileRoute } from './Routes'
+
+export const ApplicantLayout = props => (
+    <Layout
+        title={'Applicant'}
+        links={[
+            <EventsLink />,
+            <ProfileLinks
+                user={props.currentUser}
+                logout={props.logout} />
+        ]}
+        routes={[
+            <EventsRoutes />,
+            <ProfileRoute user={props.currentUser} logout={props.logout} />
+        ]}
+    />
+)
+
+export default ApplicantLayout
