021d8e76948e4298309e17d62dadc741b72e91a1
Luis Carlos Flores <luiscfgmay294@gmail.com>
2018-11-09 10:03:42 -0600

    Update API used in frontend

--- src/app/logic/actions/thunks/helper.js
+++ src/app/logic/actions/thunks/helper.js
@@ -4,8 +4,8 @@ import { createAction } from 'Logic/actions'
 
 export const request = (
     axios.create({
-        //baseURL: 'https://5a8e3738b5a3130012909abb.mockapi.io/api',
-        baseURL: 'http://cartelerai-api.herokuapp.com'
+        //baseURL: 'http://0.0.0.0:3000'
+        baseURL: 'https://cartelerai-api.herokuapp.com'
     })
 )
 
