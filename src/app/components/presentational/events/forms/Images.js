d9f255ef8c2cbf8d51d7864a13ea13a6c0d7456d
Diego JV <diegojosevilla@outlook.com>
2018-11-25 12:39:33 -0600

    Add notes for Image section

--- src/app/components/presentational/events/forms/Images.js
+++ src/app/components/presentational/events/forms/Images.js
@@ -2,12 +2,25 @@ import React, { Fragment } from 'react'
 import { ImageUploader } from 'Presentational/elements'
 
 export const EventsFormsImages = (props) => (
-    <div className="add-photos-container">
-        <ImageUploader
-            label='photo'
-            {...props} />
-        <ImageUploader
-            label='schedule'
-            {...props} />
+    <div>
+        <div className="form-general-data">
+            <h3>Notas:</h3>
+
+            <ul>
+                <li>Utiliza imagenes en formato png, jpg, jpeg y gif.</li>
+                <li>El tamaño de la imagen no debe exceder las dimensiones de 800 x 800 pixeles.</li>
+            </ul>
+        </div>
+        
+        <br></br>
+
+        <div className="add-photos-container">
+            <ImageUploader
+                label='photo'
+                {...props} />
+            <ImageUploader
+                label='schedule'
+                {...props} />
+        </div>
     </div>
 )
